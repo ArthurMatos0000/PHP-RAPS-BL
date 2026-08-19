bd5a3b747baee61fac095dff40c9419e32114657
A3thurM4tos <arthurmalmeida2@gmail.com>
2026-08-19 09:27:00 -0300

    Base sendo pronta
    aaaaaaaaaaaa

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <header class="site-header">
+    <img id="Principal" src="https://i.postimg.cc/rpVpSgb8/0554d677c32e3d870add44a15e695764.jpg">
     <nav class="main-nav">
         <ul>
             <li><a href="#">Home</a></li>
---
+++ main.php
@@ -0,0 +1,20 @@
+
+    <?php include 'header.php'; ?>
+
+    <main>
+        <div class="Caixa1">
+        <h1>Destaque da Semana</h1>
+
+        <div class="destaque-conteudo">
+            <img src="https://i.postimg.cc/90tPNLWY/gols-e-travessuras.jpg">
+
+            <div class="destaque-texto">
+                <h2>Gols e Travessuras</h2>
+                <p>Uma análise da música "Gols e Travessuras", suas letras e o impacto cultural.</p>
+                <button href="#">LEIA MAIS</button>
+            </div>
+        </div>
+    </div>
+    </main>
+
+    <?php include 'footer.php'; ?>
