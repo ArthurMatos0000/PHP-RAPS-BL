e5fa1514a9e17a7428188bdb6ea165a33d1cd180
A3thurM4tos <arthurmalmeida2@gmail.com>
2026-08-19 08:06:34 -0300

    Acho que foi finalizado
    Veremos

---
+++ header.php
@@ -0,0 +1,11 @@
+<header class="site-header">
+    <nav class="main-nav">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Marketing</a></li>
+            <li><a href="#">Internet</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+</header>
